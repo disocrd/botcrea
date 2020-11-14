5768e5464fe93895e75a63eae8f252191343a5a4
junhong123a <53944115+junhong123a@users.noreply.github.com>
2020-11-15 00:37:07 +0900

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,20 @@
+elif message.content == "?도움말":
+    embed=discord.Embed(colour=0x3AFF01)
+    embed.add_field(name="Kaiabot help", value="카야봇 도움말\n자세한내용은 공식 디스코드 참조", inline=False)
+    embed.add_field(name="게임", value="""
+    ```
+    >송금 @mention\n금액\n>도박 금액\n>가입
+    ```""", inline=True)
+    embed.add_field(name="정보", value="""
+    ```
+    >프로필\n>서버\n>정보
+    ```""", inline=True)
+    embed.add_field(name="관리", value="""
+    ```
+    >삭제 삭제할 양\n>킥 @mention\n>밴 @mention
+    ```""", inline=True)
+    embed.add_field(name="관리", value="""
+    ```
+    >삭제 삭제할 양\n>킥 @mention\n>밴 @mention
+    ```""", inline=False)
+    await message.channel.send(embed=embed)
